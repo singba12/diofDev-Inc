3b7bc747ca9e7e0e43c35b57362ed64e257e3222
singba12 <145152665+singba12@users.noreply.github.com>
2026-04-05 11:27:13 +0000

    Update app.obf.js

--- app.obf.js
+++ app.obf.js
@@ -1,77 +1,94 @@
 (function(){
+    // ===[ Table de correspondances obfusquée ]===
+    var _0x2a3b = {
+        '0x0': 'addEventListener',
+        '0x1': 'contextmenu',
+        '0x2': 'preventDefault',
+        '0x3': 'keydown',
+        '0x4': 'key',
+        '0x5': 'ctrlKey',
+        '0x6': 'shiftKey',
+        '0x7': 'outerWidth',
+        '0x8': 'innerWidth',
+        '0x9': 'outerHeight',
+        '0xa': 'innerHeight',
+        '0xb': 'body',
+        '0xc': 'innerHTML',
+        '0xd': 'location',
+        '0xe': 'href',
+        '0xf': 'about:blank',
+        '0x10': 'userAgent',
+        '0x11': 'test',
+        '0x12': 'UTC',
+        '0x13': 'now',
+        '0x14': 'market://details?id=com.diofdev.compo',
+        '0x15': 'https://play.google.com/store/apps/details?id=com.diofdev.compo',
+        '0x16': 'iPhone|iPad|iPod',
+        '0x17': 'https://youtube.com/shorts/QKqslBnI77I?si=4nrxqvGhcDP7Lfxk'
+    };
+    function _0x4c1d(_0x1a2b){
+        return _0x2a3b[_0x1a2b];
+    }
+
     // ===[ Anti-debug / Anti-inspection ]===
-    // Blocage clic droit
-    document[_0x3e2b('0x0')](_0x3e2b('0x1'), function(_0x26b1e7){
-        _0x26b1e7[_0x3e2b('0x2')]();
+    document[_0x4c1d('0x0')](_0x4c1d('0x1'), function(_0x3e4f){
+        _0x3e4f[_0x4c1d('0x2')]();
         return !![];
     });
-    // Blocage touches DevTools
-    document[_0x3e2b('0x0')](_0x3e2b('0x3'), function(_0x15c4ab){
-        var _0x2b3c4a = _0x15c4ab[_0x3e2b('0x4')];
-        if(_0x2b3c4a === 'F12' || (_0x15c4ab[_0x3e2b('0x5')] && _0x15c4ab[_0x3e2b('0x6')] && (_0x2b3c4a === 'I' || _0x2b3c4a === 'J')) || (_0x15c4ab[_0x3e2b('0x5')] && _0x2b3c4a === 'U')){
-            _0x15c4ab[_0x3e2b('0x2')]();
+    document[_0x4c1d('0x0')](_0x4c1d('0x3'), function(_0x5d6e){
+        var _0x7f8g = _0x5d6e[_0x4c1d('0x4')];
+        if(_0x7f8g === 'F12' || (_0x5d6e[_0x4c1d('0x5')] && _0x5d6e[_0x4c1d('0x6')] && (_0x7f8g === 'I' || _0x7f8g === 'J')) || (_0x5d6e[_0x4c1d('0x5')] && _0x7f8g === 'U')){
+            _0x5d6e[_0x4c1d('0x2')]();
             return ![];
         }
     });
 
-    // Détection console ouverte (via dimensions)
-    var _0x4f8a2d = ![];
-    var _0x3c71b2 = 0xa0;
-    var _0x77e3c1 = function(){
-        var _0x1e4b2a = window[_0x3e2b('0x7')] - window[_0x3e2b('0x8')] > _0x3c71b2;
-        var _0x2f8d3c = window[_0x3e2b('0x9')] - window[_0x3e2b('0xa')] > _0x3c71b2;
-        if(_0x1e4b2a || _0x2f8d3c){
-            _0x4f8a2d = !![];
-            document[_0x3e2b('0xb')][_0x3e2b('0xc')] = '';
-            window[_0x3e2b('0xd')][_0x3e2b('0xe')] = _0x3e2b('0xf');
+    var _0x9h0i = ![];
+    var _0x1j2k = 0xa0;
+    var _0x3l4m = function(){
+        var _0x5n6o = window[_0x4c1d('0x7')] - window[_0x4c1d('0x8')] > _0x1j2k;
+        var _0x7p8q = window[_0x4c1d('0x9')] - window[_0x4c1d('0xa')] > _0x1j2k;
+        if(_0x5n6o || _0x7p8q){
+            _0x9h0i = !![];
+            document[_0x4c1d('0xb')][_0x4c1d('0xc')] = '';
+            window[_0x4c1d('0xd')][_0x4c1d('0xe')] = _0x4c1d('0xf');
         }
     };
-    setInterval(_0x77e3c1, 0x3e8);
+    setInterval(_0x3l4m, 0x3e8);
 
-    // ===[ Détection téléphone portable ]===
-    function _0x2e7b3c(){
-        var _0x4f1e2a = navigator[_0x3e2b('0x10')];
-        var _0x1c3d4b = /Mobi|Android|iPhone|BlackBerry|Windows Phone/i[_0x3e2b('0x11')](_0x4f1e2a);
-        var _0x5a6b7c = window[_0x3e2b('0x8')] <= 0x300;
-        return _0x1c3d4b && _0x5a6b7c;
+    // ===[ Détection mobile ]===
+    function _0xr1s2(){
+        var _0t3u4 = navigator[_0x4c1d('0x10')];
+        var _0v5w6x = /Mobi|Android|iPhone|BlackBerry|Windows Phone/i[_0x4c1d('0x11')](_0t3u4);
+        var _0y7z8a = window[_0x4c1d('0x8')] <= 0x300;
+        return _0v5w6x && _0y7z8a;
+    }
+
+    // ===[ Détection iOS ]===
+    function _0a9b0c(){
+        var _0d1e2f = navigator[_0x4c1d('0x10')];
+        return new RegExp(_0x4c1d('0x16'))[_0x4c1d('0x11')](_0d1e2f);
     }
 
     // ===[ Condition date après 05 avril 2026 11h00 GMT ]===
-    function _0x7d8e9f(){
-        var _0x2c1b4a = Date[_0x3e2b('0x12')](2026, 0x3, 0x5, 0xb, 0x0, 0x0);
-        return Date[_0x3e2b('0x13')]() >= _0x2c1b4a;
+    function _0b9c0d(){
+        var _0e1f2g = Date[_0x4c1d('0x12')](2026, 0x3, 0x5, 0xb, 0x0, 0x0);
+        return Date[_0x4c1d('0x13')]() >= _0e1f2g;
     }
 
-    // ===[ Redirection si conditions remplies ]===
-    if(_0x2e7b3c() && _0x7d8e9f()){
-        window[_0x3e2b('0xd')][_0x3e2b('0xe')] = _0x3e2b('0x14');
-    }
-
-    // ===[ Table de correspondances hexadécimales (obfuscation) ]===
-    function _0x3e2b(_0x1a2b3c){
-        var _0x4c5d6e = {
-            '0x0': 'addEventListener',
-            '0x1': 'contextmenu',
-            '0x2': 'preventDefault',
-            '0x3': 'keydown',
-            '0x4': 'key',
-            '0x5': 'ctrlKey',
-            '0x6': 'shiftKey',
-            '0x7': 'outerWidth',
-            '0x8': 'innerWidth',
-            '0x9': 'outerHeight',
-            '0xa': 'innerHeight',
-            '0xb': 'body',
-            '0xc': 'innerHTML',
-            '0xd': 'location',
-            '0xe': 'href',
-            '0xf': 'about:blank',
-            '0x10': 'userAgent',
-            '0x11': 'test',
-            '0x12': 'UTC',
-            '0x13': 'now',
-            '0x14': 'https://play.google.com/store/apps/details?id=com.diofdev.compo'
-        };
-        return _0x4c5d6e[_0x1a2b3c];
+    // ===[ Redirection conditionnelle ]===
+    if(_0r1s2() && _0b9c0d()){
+        if(_0a9b0c()){
+            // iOS : redirection vers le tutoriel YouTube
+            window[_0x4c1d('0xd')][_0x4c1d('0xe')] = _0x4c1d('0x17');
+        } else {
+            // Android (ou autre) : deep link Play Store + fallback
+            window[_0x4c1d('0xd')][_0x4c1d('0xe')] = _0x4c1d('0x14');
+            setTimeout(function(){
+                if(document[_0x4c1d('0xb')] && document[_0x4c1d('0xb')][_0x4c1d('0xc')] !== ''){
+                    window[_0x4c1d('0xd')][_0x4c1d('0xe')] = _0x4c1d('0x15');
+                }
+            }, 0x1f4);
+        }
     }
 })();
